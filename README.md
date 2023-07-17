7f0f93ad4fd20cc3f8281edfaef4d950d09df3d4
ngocminhta <86149296+ngocminhta@users.noreply.github.com>
2023-07-17 09:54:07 +0700

    final push

--- README.md
+++ README.md
@@ -3,10 +3,20 @@ Stock Price Prediction using ARIMA
 This work is a mini-project under the course IT2022E - Applied Statistics and Experimental Design.
 Supervisor: Assoc. Prof. NGUYEN Linh Giang
 
-Reference
-=========
+This repository contains all folders of our project about "Stock Price Analysis and Prediction".
+============
 
-<span>1.</span> H.Q.Thang. *Vietnam Stock Index Trend Prediction using
-Gaussian Process Regression and Autoregressive Moving Average Model*.
-Research and Development on Information and Communication Technology,
-HUST, 2018.
+![image](https://github.com/tttruong0812/Capstone-Project-ML/assets/95478230/eb5cb261-5759-4937-a9a5-cff1cba66229)
+
+
+Dataset is attached in the folder "Dataset", that includes some stocks in diffrent fields (construction, finance, consumer, technology).
+
+We analyze some indicators like RSI, buy or sell signal in folder "Data Analysis". You can access this to see implementation, some properties of dataset and our prediction about buy or sell  indicated RSI.
+
+About machine learning models, we use three algorithms that are ARIMA, Support Vector Regression (SVR), Extreme Gradient Boosting (XGBoost) to train, deploy and evaluate model, then give predictions about price of stock.
+
+Before using algorithms to train model, we created data preprocessing, cleaning, and visualization for easier implementation.
+
+In ARIMA model, we apply some test statistics to check if data is stationary or not, and then choose the parameters to train and evaluate model.
+
+In folder SVR and XGBoost, we try to  model selection and choose optimal hyperparameters by using Nested Cross Validation Time Series (Predict-Second-Half and Day-Forward-Chaining). After that, we use that hyperparameters to retrain, evaluate model and predict price of stocks.
---
+++ SVR/DataPreprocessing.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Import Libraries
+
+# In[1]:
+
+
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import datetime as dt
+
+from xgboost import XGBRegressor
+from sklearn.svm import SVR
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score 
+from sklearn.metrics import mean_poisson_deviance, mean_gamma_deviance, accuracy_score
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import learning_curve, train_test_split
+
+from itertools import cycle
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+from tqdm import tqdm
+
+import seaborn as sns
+sns.set_style('whitegrid')
+plt.style.use("fivethirtyeight")
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# ## Load dataset
+
+# In[2]:
+
+
+df = pd.read_csv('C:/Users/Admin/Desktop/Capstone-Project-ML/Dataset/technology/VGI.csv', sep='\t', index_col=0)
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[4]:
+
+
+df.info()
+
+
+# In[5]:
+
+
+df.describe()
+
+
+# ## Data Cleaning
+
+# In[6]:
+
+
+df['TradingDate'] = pd.to_datetime(df['TradingDate'], format='%Y-%m-%d')
+
+
+# In[7]:
+
+
+df = df.dropna()
+
+
+# ## Data Visualization
+
+# In[8]:
+
+
+plt.figure(figsize=(16,6))
+plt.title('Close Price History')
+plt.plot(df['Close'])
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price USD ($)', fontsize=18)
+plt.show()
+
+
+# ## Manipulate data
+
+# In[9]:
+
+
+# Create a new dataframe
+data = df.filter(['Close'])
+# Convert the dataframe to a numpy array
+dataset = data.values
+
+
+# In[10]:
+
+
+# scaler = MinMaxScaler(feature_range=(0,1))
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(dataset)
+
+scaled_data
+
+
+# In[ ]:
+
+
+
+
